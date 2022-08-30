2f7b9d7cefe90aa933546a5af981ca13d6baf620
Cameron White <cameronwhite35288@gmail.com>
2022-08-30 14:35:59 -0400

    Update Lab3A.java

--- Lab3A.java
+++ Lab3A.java
@@ -20,7 +20,6 @@ public class Lab3A {
         mpc = owed * apr / 12 / 100;
         String mpr = String.format("%.3f", mprc);
         String mp = String.format("%.2f", mpc);
-        System.out.println("Amount owed: $" + Math.round(owed));
         System.out.println("APR: " + apr);
         System.out.println("Monthly Percentage Rate: " + mpr);
         System.out.println("Minimum payment: $" + mp);
